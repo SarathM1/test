6a756d9f908b5a721811fe21f765163ce7457d0a
Wirewords Academy <sarathm00@gmail.com>
2015-03-06 16:53:59 +0530

    "Done"

---
+++ addRemote.py
@@ -0,0 +1,5 @@
+from subprocess import call as c
+com1c = "git remote add ssh3 git@github.com:SarathM1/test"
+
+
+c(com1c.split())
--- commitCode.py
+++ commitCode.py
@@ -4,6 +4,7 @@ from time import sleep
 com1p = 'git status'
 com2c = 'git add --all'
 com3c = 'git commit -m "Done"'
+com4c = 'git push ssh3'
 
 p1 = p(com1p.split())
 p1.wait()
@@ -15,3 +16,6 @@ print "-------------------------------"
 sleep(2)
 p3 = c(com3c.split())
 #p3.wait()
+print "-------------------------------"
+sleep(2)
+p4 = c(com4c.split())
